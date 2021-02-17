41ac2ef64442e2475ea276e14891ed54257be8af
Jose Vte. Díaz Mora <74594028+JVDMKKHHPMY@users.noreply.github.com>
2021-02-17 21:26:14 +0100

    Añado el codigo que hicimos en equipo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-const getMovieHtml=movie=>{
+const getMovieHtml = movie=> {
     return `<div class = "movie" onclick="getMovieDetailed(${movie.id})">
         <h3>${movie.title}</h3>
-        <img src ="https://image.tmdb.org/t/p/w185/${movie.poster_path}" alt ="">
-    </div>`
+        <img src ="https://image.tmdb.org/t/p/w185/${movie.poster_path}" alt ="imagen de la película">
+    </div>
+    `
+}
+
+const getMovieDetailedHtml = movie => {
+    return ` <div class = "movie" onclick="getMovieDetailed(${movie.id})">
+        <h3>${movie.title}</h3>
+        <img src ="https://image.tmdb.org/t/w300/p${movie.poster_path}" alt ="imagen de la pelicula">
+        <br>
+        <span>$Popularidad:{movie.popularity}</span>
+        <p>${movie.overview}</p>
+    </div>
+    `
 }
 
 
@@ -12,8 +24,6 @@ const renderMovies = movies => {
     for (const movie of movies) {
         document.querySelector('main.movies').innerHTML += getMovieHtml(movie)
         
-        
-
     }
     
 }
@@ -23,6 +33,7 @@ const renderMovies = movies => {
 const getMovieDetailed = movie_id =>{
     axios.get(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=b5138e06a3a9125b8c326498bbeae997&language=es-ES`).then(res=>{
         const movie = res.data;
+        document.querySelector('main.movies').innerHTML = getMovieDetailedHtml(movie)
 
     })
     .catch(console.error)
@@ -55,3 +66,86 @@ const getPopularMovies = () => {
     })
     .catch(error=>console.error(error))
 }
+
+
+
+/*//funciones
+let valor = document.getElementById("buscador");
+let valorId = document.getElementById("buscadorID");
+//Recursos API
+
+let key = "be761d96072c0447097947c70d2686b6";
+
+let recurso= "search";
+
+let criterio = "movie";
+
+let base_url = `https://api.themoviedb.org/3/`;
+
+
+const call = async (url) => {
+    let res = await axios.get(url);
+    
+
+    if (res.data.results){
+        return res.data.results;
+
+    }
+    if (res.data.title){
+        return res.data;
+    }
+    
+}
+
+
+const pintar = async (coleccionPintar) => {
+    //Proceso para el pintado HTML de las películas
+    const divPelisDomElement = document.getElementById("contenedor");
+    
+    // console.log(coleccionPintar);
+    if (Array.isArray(coleccionPintar)) {
+        console.log(coleccionPintar);
+        coleccionPintar.forEach(pelicula => {
+            divPelisDomElement.innerHTML += `<div id='peliculas'>
+            <img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}'></img></div>
+            <div id='textos'><h2>${pelicula.title}</h2>
+            <p>${pelicula.overview}</p>
+            </div>`
+        });
+    }
+    
+    if (coleccionPintar.original_title){
+        divPelisDomElement.innerHTML += `<div class='peliculas'>
+            <img src='https://image.tmdb.org/t/p/w500${coleccionPintar.poster_path}' width='200px' class='picture'>
+            </img></div><div class='infoPelis'><h2>${coleccionPintar.original_title}</h2><p>${coleccionPintar.overview}</p></div>`
+        
+    }
+    return;
+};
+
+
+
+const buscador = async () => {
+    let query = valor.value;
+    //Construccion de la URL 
+    let url = `${base_url}${recurso}/${criterio}?api_key=${key}&language=es-ES&query=${query}`; 
+    pintar("cargando");
+    let pelis = await call(url);
+
+
+    pintar(pelis);
+        
+};
+const buscadorID = async () => {
+    let query = valorId.value;
+    
+    //Construccion de la URL 
+    
+    let url = `${base_url}${criterio}/${query}?api_key=${key}&language=es-ES`; 
+    pintar("cargando");
+    let pelis = await call(url);
+
+
+    pintar(pelis);
+        
+};
